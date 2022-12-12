cb812ed2da1a3248f4ee3b0dfb7c5f767c791c78
Diogo Neves Soares <113932801+DiogoSoares3@users.noreply.github.com>
2022-12-12 17:43:23 -0300

    Update view_sistema.py

--- view/view_sistema.py
+++ view/view_sistema.py
@@ -1,25 +1,41 @@
-class TelaSistema:
-    def le_num_inteiro(self, mensagem=" ", ints_validos=None):
-        while True:
-            valor_lido = input(mensagem)
-            try:
-                valor_int = int(valor_lido)
-                if ints_validos and valor_int not in ints_validos:
-                    raise ValueError
-                return valor_int
-            except ValueError:
-                print("Valor incorreto!")
-                if ints_validos:
-                    print("Valores válidos: ", ints_validos)
+import PySimpleGUI as sg
 
+class TelaSistema:
+    def __init__(self):
+        self.__window = None
+        self.init_components()
+    
     def tela_opcoes(self):
-        print("-------- Sistema ---------")
-        print("Escolha sua opção")
-        print("1 - Lutas")
-        print("2 - Lutadores")
-        print("3 - Narradores")
-        print("4 - Campeonato")
-        print("0 - Finalizar sistema")
-        print("--------------------------")
-        opcao = self.le_num_inteiro("Escolha a opção:", [0, 1, 2, 3, 4])
+        self.init_components()
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['4']:
+            opcao = 4
+        if values['0'] or button in (None,'Cancelar'):
+            opcao = 0
+        self.close()
         return opcao
+
+    def init_components(self):
+        #sg.theme_previewer()
+        sg.ChangeLookAndFeel('DarkTeal4')
+        layout = [
+            [sg.Text('Bem vindo ao sistema de Campeonato de MMA!', font=("Helvica",25))],
+            [sg.Text('Escolha sua opção', font=("Helvica",15))],
+            [sg.Radio('Lutas',"RD1", key='1')],
+            [sg.Radio('Lutadores',"RD1", key='2')],
+            [sg.Radio('Narradores',"RD1", key='3')],
+            [sg.Radio('Campeonato',"RD1", key='4')],
+            [sg.Radio('Finalizar sistema',"RD1", key='0')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Sistema de MMA').Layout(layout)
+        
+    def close(self):
+        self.__window.Close()
