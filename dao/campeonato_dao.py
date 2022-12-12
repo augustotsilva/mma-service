9e7184d0ea51bd4dc6ff2e297d272829c88e4e7b
augustotsilva <augusto.silva@netpos.com.br>
2022-12-12 15:15:27 -0300

    dao - augusto
    # wip DAO

--- dao/campeonato_dao.py
+++ dao/campeonato_dao.py
@@ -18,6 +18,6 @@ class CampeonatoDAO(BaseDAO):
         if isinstance(key, int):
             return super().get(key)
 
-    def remove(selfself, key: int):
+    def remove(self, key: int):
         if isinstance(key, int):
             return super().remove(key)
